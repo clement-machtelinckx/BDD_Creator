c6a706c2932f2ee6b93eb1dac79b35576107ef7b
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2024-08-31 16:31:40 +0200

    a l'aide

--- src/class/Database.php
+++ src/class/Database.php
@@ -8,7 +8,7 @@ class Database
     private $dbName;
     private $pdo;
 
-    public function __construct($host, $username, $password, $dbName)
+    public function __construct($host, $username, $password, $dbName = null)
     {
         $this->host = $host;
         $this->username = $username;
@@ -19,13 +19,82 @@ class Database
     public function connect()
     {
         try {
-            $this->pdo = new PDO("mysql:host={$this->host};dbname={$this->dbName}", $this->username, $this->password);
+            $dsn = "mysql:host={$this->host}";
+            if ($this->dbName) {
+                $dsn .= ";dbname={$this->dbName}";
+            }
+            $this->pdo = new PDO($dsn, $this->username, $this->password);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
             die("Connection failed: " . $e->getMessage());
         }
     }
+
+    public function createDatabase($databaseName)
+    {
+        $sql = "CREATE DATABASE $databaseName";
+        if ($this->pdo->exec($sql)) {
+            echo "Database created successfully";
+        } else {
+            echo "Error creating database: " . $this->pdo->errorInfo();
+        }
+    }
+
+    public function showDatabases()
+    {
+        $sql = "SHOW DATABASES";
+        $stmt = $this->pdo->query($sql);
+        $databases = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        return $databases;
+    }
+
+    public function createTable($tableName, $dbName)
+    {
+        $sql = "CREATE TABLE $dbName.$tableName (
+            id INT(6) AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
+            reg_date TIMESTAMP
+            )";
+        if ($this->pdo->exec($sql) == true) {
+            echo "Table created successfully";
+        } else {
+            $errorInfo = $this->pdo->errorInfo();
+            if ($errorInfo[0] !== '00000') {
+                echo "Error creating table: " . implode(' ', $errorInfo);
+            }
+        }
+    }
+
+    public function modifyTable($tableName, $dbName, $columnName, $columnType)
+    {
+        $sql = "ALTER TABLE $dbName.$tableName MODIFY COLUMN $columnName $columnType";
+        if ($this->pdo->exec($sql)) {
+            echo "Table modified successfully";
+        } else {
+            $errorInfo = $this->pdo->errorInfo();
+            if ($errorInfo[0] !== '00000') {
+                echo "Error modifying table: " . implode(' ', $errorInfo);
+            }
+        }
+    }
+
+    public function addColumn($tableName, $dbName, $columnName, $columnType)
+    {
+        $sql = "ALTER TABLE $dbName.$tableName ADD COLUMN $columnName $columnType";
+        if ($this->pdo->exec($sql)) {
+            echo "Column added successfully";
+        } else {
+            $errorInfo = $this->pdo->errorInfo();
+            if ($errorInfo[0] !== '00000') {
+                echo "Error adding column: " . implode(' ', $errorInfo);
+            }
+        }
+    }
+    
+    
 
     public function getPdo()
     {
@@ -35,6 +104,6 @@ class Database
 
 // Usage:
 // host / username / password / dbname
-$db = new Database('localhost', 'root', '', 'blog');
+$db = new Database('localhost', 'root', '');
 $db->connect();
 $pdo = $db->getPdo();
--- src/pages/index.php
+++ src/pages/index.php
@@ -1,139 +1,29 @@
 <?php
+include_once '../class/Database.php';
+// Usage:
+// host / username / password / dbname
+$db = new Database('localhost', 'root', '');
+$db->connect();
+// $db->addColumn('zz', 'test', 'coucou', 'VARCHAR(200)');
 
-$hostname = "localhost";
-$username = "root";
-$password = "";
-
-
-// showDatabases($connect);
-var_dump($_POST);
-var_dump($_GET);
-
-
-//if add BDD is set, create database
+var_dump($pdo);
 if (isset($_POST['addBDD'])) {
     $databaseName = $_POST['addBDD'];
-    $connect = mysqli_connect($hostname, $username, $password,);
-    createDatabase($connect, $databaseName);
+    $db->createDatabase($databaseName);
 }
-//if addTable is set, create table in database bdName
+var_dump($_POST['addBDD']);
+
 if (isset($_POST['addTable']) && isset($_POST['dbName'])) {
     $tableName = $_POST['addTable'];
     $dbName = $_POST['dbName'];
-    $connect = mysqli_connect($hostname, $username, $password, $dbName);
-    createTable($connect, $tableName, $dbName);
-}
-
-
-function createDatabase($connect, $databaseName)
-{
-    $sql = "CREATE DATABASE $databaseName";
-    if (mysqli_query($connect, $sql)) {
-        echo "Database created successfully";
-    } else {
-        echo "Error creating database: " . mysqli_error($connect);
-    }
-}
-function createTable($connect, $tableName, $dbName)
-{
-    $sql = "CREATE TABLE $dbName.$tableName (
-        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        firstname VARCHAR(30) NOT NULL,
-        lastname VARCHAR(30) NOT NULL,
-        email VARCHAR(50),
-        reg_date TIMESTAMP
-        )";
-    if (mysqli_query($connect, $sql)) {
-        echo "Table created successfully";
-    } else {
-        echo "Error creating table: " . mysqli_error($connect);
-    }
-}
-function showDatabases($connect)
-{
-    $sql = "SHOW DATABASES";
-    $result = mysqli_query($connect, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo $row["Database"] . "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-}
-
-function getDatabases($connect)
-{
-    $sql = "SHOW DATABASES";
-    $result = mysqli_query($connect, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $databases[] = $row["Database"];
-        }
-    } else {
-        echo "0 results";
-    }
-    return $databases;
-}
-function getTables($connect, $dbName)
-{
-    $sql = "SHOW TABLES FROM $dbName";
-    $result = mysqli_query($connect, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $tables[] = $row["Tables_in_" . $dbName];
-        }
-    } else {
-        echo "0 results";
-    }
-    return $tables;
-}
-
-function showTables($connect, $dbName)
-{
-    $sql = "SHOW TABLES FROM $dbName";
-    $result = mysqli_query($connect, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo $row["Tables_in_" . $dbName] . "<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-}
-function showTableData($connect, $tableName, $dbName)
-{
-    $sql = "SELECT * FROM $dbName.$tableName";
-    $result = $connect->query($sql);
-
-    if ($result->rowCount() > 0) {
-        while ($row = $result->fetch()) {
-            echo $row["firstname"] . " " . $row["lastname"] . " (" . $row["email"] . ")<br>";
-        }
-    } else {
-        echo "0 results";
-    }
-}
-function insertData($connect, $tableName, $dbName, $firstname, $lastname, $email)
-{
-    $sql = "INSERT INTO $dbName.$tableName (firstname, lastname, email)
-    VALUES ('$firstname', '$lastname', '$email')";
-    if ($connect->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $connect->error;
-    }
+    $db->createTable($tableName, $dbName);
 }
 
 ?>
 <!DOCTYPE html>
 
 
-<form method="POST">
+<form action="" method="POST">
     <label for="addBDD">Create database</label>
     <input type="text" id="addBDD" name="addBDD">
     <input type="submit" value="Enter">
@@ -147,15 +37,17 @@ function insertData($connect, $tableName, $dbName, $firstname, $lastname, $email
     <input type="submit" value="Enter">
 </form>
 
-<form method="GET" action="selectedBDD.php">
+<form method="GET" action="">
 <label for="selectBDD"> selectBDD</label>
 <select name="selectBDD" id="selectBDD">
     <?php
-    $connect = mysqli_connect($hostname, $username, $password);
-    $databases = getDatabases($connect);
+
+    $databases = $db->showDatabases();
+    var_dump($databases);
     foreach ($databases as $database) {
         echo "<option value='$database'>$database</option>";
     }
+
     ?> 
 </select>
 <input type="submit" value="Enter">
