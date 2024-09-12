dbf727b2c4697b6299bb7b0929377754b73dba9f
clement-machtelinckx <clement.machtelinckx@laplateforme.io>
2024-09-12 16:49:13 +0200

    start test with cron

--- src/index.php
+++ src/index.php
@@ -13,5 +13,19 @@ $dir = dirname(__FILE__). '/dump/';
 
 var_dump($dir);
 
+
 scandir($dir);
-var_dump(scandir($dir));
+// var_dump(scandir($dir));
+
+$n = 0;
+
+foreach (scandir($dir) as $file) {
+    if ('.' === $file) continue;
+    if ('..' === $file) continue;
+    $dir = 'C:\wamp64\www\BDD_Creator\src/dump/' . $file;
+    $databaseName = "test" . $n +=1;
+    exec("mysqldump --user={$USERNAME} --password={$PASSWORD} --host={$HOST} {$databaseName} --result-file={$dir} 2>&1", $output);
+    // echo $databaseName;
+    // echo $output;
+    echo $file ;
+}
